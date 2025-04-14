cc6b79d86446d7e63ace0c710144160e49c3b7dc
Kevin Lourd <kevin.lourd@gmail.com>
2025-04-14 21:00:12 +0700

    updated agent service

--- apps/agent-service/src/lib/promptBuilder.ts
+++ apps/agent-service/src/lib/promptBuilder.ts
@@ -11,10 +11,14 @@ import { User, AgentRecord } from '@agent-base/agents';
  */
 export const available_tools_prompt = `
 ### Available tools: 
-- utility_list_utilities: List all utilities available for you to call. 
-- utility_get_utility_info: Get information about a specific utility you may want to call. 
-- utility_call_utility: Call a specific utility with the utility id and provided arguments. 
+1- utility_list_utilities: List all utilities available for you to call. 
+2- utility_get_utility_info: Get information about a specific utility you may want to call. 
+3- utility_call_utility: Call a specific utility with the utility id and provided arguments.
 Don't call utility_id directly. Instead call the utility_call_utility tool with the utility_id you wan to be called.
+
+### Important internal tools to use as often as you need (to be executed with utility_call_utility):
+- utility_create_external_tool: Create a new external tool. Everytime the user wants you to do an action,
+and you can't find the right tool to do it, use this tool to create a new external tool based on an API Documentation from your knowledge or found online.
 `;
 export const purpose_prompt = `
 ### Purpose: 
